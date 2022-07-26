f226abf74709a8321350ef7bfc4ca375911a17fb
jpcconsultings <109997845+jpcconsultings@users.noreply.github.com>
2022-07-25 19:51:31 -0500

    ProyectoFinal
    Archivos proyecto final

---
+++ PrimerDesafio/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Usuario{
+    private int Id;
+    private string Nombre;
+    private string Apellido;
+    private string NombreUsuario;
+    private string Contrasenia;
+    private string Mail;
+}
+
+public class Producto
+{
+    private int Id;
+    private string Descripcion;
+    private long Costo;
+    private long PrecioVenta;
+    private long Stock;
+    private int IdUsuario;
+
+}
+
+public class ProductoVendido
+{
+    private int Id;
+    private int IdProducto;
+    private int Stock;
+    private int IdVenta;
+
+}
+
+public class Venta
+{
+    private int Id;
+    private string Comentarios;
+
+}
